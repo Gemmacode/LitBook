f7d0c87ae27821b8787f66f796fddb3a099de875
Gemma <jemmimah.abdul@decagon.dev>
2023-10-27 20:45:12 +0100

    updated Project

--- LitBookWeb/LitBook/Controllers/CategoryController.cs
+++ LitBookWeb/LitBook/Controllers/CategoryController.cs
@@ -1,12 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LitBook.Data;
+using LitBook.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace LitBook.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            List<Category> categoryList = _context.Categories.ToList();
+            return View(categoryList);
         }
     }
 }
