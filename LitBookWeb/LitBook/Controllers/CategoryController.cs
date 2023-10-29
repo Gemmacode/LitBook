531f3c97a4d4189747c1010202f992651f00064e
Gemma <jemmimah.abdul@decagon.dev>
2023-10-29 03:04:11 +0100

    Updated Project

--- LitBookWeb/LitBook/Controllers/CategoryController.cs
+++ LitBookWeb/LitBook/Controllers/CategoryController.cs
@@ -27,11 +27,42 @@ namespace LitBook.Controllers
         [HttpPost]
         public IActionResult Create(Category obj)
         {
-            //if (obj.Name == obj.DisplayOrder.ToString())
-            //{
-            //    ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the name.");
-            //}
-           
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the name.");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _context.Categories.Add(obj);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFrmDb = _context.Categories.Find(id); 
+            if (categoryFrmDb == null)
+            {
+                NotFound();
+            }
+            return View(categoryFrmDb);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the name.");
+            }
+
             if (ModelState.IsValid)
             {
                 _context.Categories.Add(obj);
